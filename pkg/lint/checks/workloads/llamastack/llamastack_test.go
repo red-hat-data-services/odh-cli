51e65dd54cef11e098d43867ea96b04f20350589
Derek Higgins <derekh@redhat.com>
2026-02-17 12:24:55 +0000

    refactor: simplify LlamaStack check to single blocking condition
    Simplifies the LlamaStack upgrade check by updating message to
      "Archive data before upgrade" and remove indivisual checks.
    
    Signed-off-by: Derek Higgins <derekh@redhat.com>
    (cherry picked from commit 619485787a4ab60899eb9ba7b43de51f74a2cca4)

--- pkg/lint/checks/workloads/llamastack/llamastack_test.go
+++ pkg/lint/checks/workloads/llamastack/llamastack_test.go
@@ -20,7 +20,6 @@ import (
 //nolint:gochecknoglobals
 var listKinds = map[schema.GroupVersionResource]string{
 	resources.LlamaStackDistribution.GVR(): resources.LlamaStackDistribution.ListKind(),
-	resources.ConfigMap.GVR():              resources.ConfigMap.ListKind(),
 	resources.DataScienceCluster.GVR():     resources.DataScienceCluster.ListKind(),
 }
 
@@ -46,29 +45,7 @@ func newDSC(componentStates map[string]string) *unstructured.Unstructured {
 	}
 }
 
-func newLLSD(name, namespace string, envVars map[string]string, configMapName string) *unstructured.Unstructured {
-	env := make([]any, 0, len(envVars))
-	for k, v := range envVars {
-		env = append(env, map[string]any{
-			"name":  k,
-			"value": v,
-		})
-	}
-
-	spec := map[string]any{
-		"server": map[string]any{
-			"containerSpec": map[string]any{
-				"env": env,
-			},
-		},
-	}
-
-	if configMapName != "" {
-		spec["server"].(map[string]any)["userConfig"] = map[string]any{
-			"configMapName": configMapName,
-		}
-	}
-
+func newLLSD(name, namespace string) *unstructured.Unstructured {
 	return &unstructured.Unstructured{
 		Object: map[string]any{
 			"apiVersion": resources.LlamaStackDistribution.APIVersion(),
@@ -77,35 +54,9 @@ func newLLSD(name, namespace string, envVars map[string]string, configMapName st
 				"name":      name,
 				"namespace": namespace,
 			},
-			"spec": spec,
-		},
-	}
-}
-
-func newConfigMap(name, namespace string, hasConfigYaml bool) *unstructured.Unstructured {
-	return newConfigMapWithContent(name, namespace, hasConfigYaml, "# valid YAML config content\nkey: value")
-}
-
-func newConfigMapWithContent(
-	name string,
-	namespace string,
-	hasConfigYaml bool,
-	content string,
-) *unstructured.Unstructured {
-	data := map[string]any{}
-	if hasConfigYaml {
-		data["config.yaml"] = content
-	}
-
-	return &unstructured.Unstructured{
-		Object: map[string]any{
-			"apiVersion": resources.ConfigMap.APIVersion(),
-			"kind":       resources.ConfigMap.Kind,
-			"metadata": map[string]any{
-				"name":      name,
-				"namespace": namespace,
+			"spec": map[string]any{
+				"server": map[string]any{},
 			},
-			"data": data,
 		},
 	}
 }
@@ -133,13 +84,6 @@ func TestLlamaStackConfigCheck_CanApply(t *testing.T) {
 			targetVersion:  "3.0.0",
 			componentState: "Managed",
 			expected:       true,
-		},
-		{
-			name:           "3.0 to 3.3 upgrade (wrong version range)",
-			currentVersion: "3.0.0",
-			targetVersion:  "3.3.0",
-			componentState: "Managed",
-			expected:       false,
 		},
 		{
 			name:           "2.25 to 3.3 upgrade with component Removed",
@@ -193,28 +137,25 @@ func TestLlamaStackConfigCheck_NoWorkloads(t *testing.T) {
 	g.Expect(err).ToNot(HaveOccurred())
 	g.Expect(res.Status.Conditions).To(HaveLen(1))
 	g.Expect(res.Status.Conditions[0].Condition).To(MatchFields(IgnoreExtras, Fields{
-		"Type":   Equal("VLLMConfigured"),
+		"Type":   Equal("RequiresRecreation"),
 		"Status": Equal(metav1.ConditionTrue),
+		"Reason": Equal(check.ReasonResourceNotFound),
 	}))
+	g.Expect(res.Status.Conditions[0].Condition.Message).To(ContainSubstring("No LlamaStackDistribution resources found"))
+	g.Expect(res.Status.Conditions[0].Impact).To(Equal(result.ImpactNone))
+	g.Expect(res.ImpactedObjects).To(BeEmpty())
 }
 
-func TestLlamaStackConfigCheck_AllRequiredConfigured(t *testing.T) {
+func TestLlamaStackConfigCheck_WorkloadsFound(t *testing.T) {
 	g := NewWithT(t)
 
-	envVars := map[string]string{
-		"VLLM_URL":           "http://vllm:8000",
-		"POSTGRES_HOST":      "postgres.default.svc",
-		"POSTGRES_PASSWORD":  "password",
-		"VLLM_EMBEDDING_URL": "http://embedding:8000",
-	}
-
 	dsc := newDSC(map[string]string{"llamastackoperator": "Managed"})
-	cm := newConfigMap("test-config", "test-ns", true)
-	llsd := newLLSD("test-llsd", "test-ns", envVars, "test-config")
+	llsd1 := newLLSD("llsd-1", "test-ns")
+	llsd2 := newLLSD("llsd-2", "test-ns")
 
 	target := testutil.NewTarget(t, testutil.TargetConfig{
 		ListKinds:      listKinds,
-		Objects:        []*unstructured.Unstructured{dsc, llsd, cm},
+		Objects:        []*unstructured.Unstructured{dsc, llsd1, llsd2},
 		CurrentVersion: "2.25.2",
 		TargetVersion:  "3.3.0",
 	})
@@ -223,236 +164,27 @@ func TestLlamaStackConfigCheck_AllRequiredConfigured(t *testing.T) {
 	res, err := chk.Validate(t.Context(), target)
 
 	g.Expect(err).ToNot(HaveOccurred())
-	g.Expect(res.Status.Conditions).To(HaveLen(4))
+	g.Expect(res.Status.Conditions).To(HaveLen(1))
 
-	// All conditions should be True
-	for _, cond := range res.Status.Conditions {
-		g.Expect(cond.Status).To(Equal(metav1.ConditionTrue))
-		g.Expect(cond.Impact).To(Equal(result.ImpactNone))
-	}
+	// Verify condition
+	g.Expect(res.Status.Conditions[0].Condition).To(MatchFields(IgnoreExtras, Fields{
+		"Type":   Equal("RequiresRecreation"),
+		"Status": Equal(metav1.ConditionFalse),
+		"Reason": Equal("ArchitecturalIncompatibility"),
+	}))
+	g.Expect(res.Status.Conditions[0].Condition.Message).To(ContainSubstring("Found 2 LlamaStackDistribution(s)"))
+	g.Expect(res.Status.Conditions[0].Condition.Message).To(ContainSubstring("must be deleted and recreated"))
+	g.Expect(res.Status.Conditions[0].Condition.Message).To(ContainSubstring("ALL DATA WILL BE LOST"))
+	g.Expect(res.Status.Conditions[0].Impact).To(Equal(result.ImpactBlocking))
+	g.Expect(res.Status.Conditions[0].Remediation).To(ContainSubstring("backup script"))
 
-	// Verify ConfigMapValid condition mentions verification
-	var cmCond *result.Condition
-	for i := range res.Status.Conditions {
-		if res.Status.Conditions[i].Type == "ConfigMapValid" {
-			cmCond = &res.Status.Conditions[i]
-
-			break
-		}
-	}
-	g.Expect(cmCond).ToNot(BeNil())
-	g.Expect(cmCond.Message).To(ContainSubstring("verify config.yaml content against llamastack documentation"))
-}
-
-func TestLlamaStackConfigCheck_MissingVLLMURL(t *testing.T) {
-	g := NewWithT(t)
-
-	envVars := map[string]string{
-		"POSTGRES_HOST":      "postgres.default.svc",
-		"POSTGRES_PASSWORD":  "password",
-		"VLLM_EMBEDDING_URL": "http://embedding:8000",
-	}
-
-	dsc := newDSC(map[string]string{"llamastackoperator": "Managed"})
-	cm := newConfigMap("test-config", "test-ns", true)
-	llsd := newLLSD("test-llsd", "test-ns", envVars, "test-config")
-
-	target := testutil.NewTarget(t, testutil.TargetConfig{
-		ListKinds:      listKinds,
-		Objects:        []*unstructured.Unstructured{dsc, llsd, cm},
-		CurrentVersion: "2.25.2",
-		TargetVersion:  "3.3.0",
-	})
-
-	chk := llamastack.NewConfigCheck()
-	res, err := chk.Validate(t.Context(), target)
-
-	g.Expect(err).ToNot(HaveOccurred())
-
-	// Find VLLMConfigured condition
-	var vllmCond *result.Condition
-	for i := range res.Status.Conditions {
-		if res.Status.Conditions[i].Type == "VLLMConfigured" {
-			vllmCond = &res.Status.Conditions[i]
-
-			break
-		}
-	}
-
-	g.Expect(vllmCond).ToNot(BeNil())
-	g.Expect(vllmCond.Status).To(Equal(metav1.ConditionFalse))
-	g.Expect(vllmCond.Impact).To(Equal(result.ImpactBlocking))
-	g.Expect(vllmCond.Message).To(ContainSubstring("missing required VLLM_URL"))
-}
-
-func TestLlamaStackConfigCheck_MissingPostgresConfig(t *testing.T) {
-	g := NewWithT(t)
-
-	envVars := map[string]string{
-		"VLLM_URL":           "http://vllm:8000",
-		"VLLM_EMBEDDING_URL": "http://embedding:8000",
-	}
-
-	dsc := newDSC(map[string]string{"llamastackoperator": "Managed"})
-	cm := newConfigMap("test-config", "test-ns", true)
-	llsd := newLLSD("test-llsd", "test-ns", envVars, "test-config")
-
-	target := testutil.NewTarget(t, testutil.TargetConfig{
-		ListKinds:      listKinds,
-		Objects:        []*unstructured.Unstructured{dsc, llsd, cm},
-		CurrentVersion: "2.25.2",
-		TargetVersion:  "3.3.0",
-	})
-
-	chk := llamastack.NewConfigCheck()
-	res, err := chk.Validate(t.Context(), target)
-
-	g.Expect(err).ToNot(HaveOccurred())
-
-	// Find PostgresConfigured condition
-	var pgCond *result.Condition
-	for i := range res.Status.Conditions {
-		if res.Status.Conditions[i].Type == "PostgresConfigured" {
-			pgCond = &res.Status.Conditions[i]
-
-			break
-		}
-	}
-
-	g.Expect(pgCond).ToNot(BeNil())
-	g.Expect(pgCond.Status).To(Equal(metav1.ConditionFalse))
-	g.Expect(pgCond.Impact).To(Equal(result.ImpactBlocking))
-	g.Expect(pgCond.Message).To(ContainSubstring("PostgreSQL configuration"))
-}
-
-func TestLlamaStackConfigCheck_InvalidConfigMap(t *testing.T) {
-	g := NewWithT(t)
-
-	envVars := map[string]string{
-		"VLLM_URL":           "http://vllm:8000",
-		"POSTGRES_HOST":      "postgres.default.svc",
-		"POSTGRES_PASSWORD":  "password",
-		"VLLM_EMBEDDING_URL": "http://embedding:8000",
-	}
-
-	dsc := newDSC(map[string]string{"llamastackoperator": "Managed"})
-	cm := newConfigMap("test-config", "test-ns", false) // Missing config.yaml
-	llsd := newLLSD("test-llsd", "test-ns", envVars, "test-config")
-
-	target := testutil.NewTarget(t, testutil.TargetConfig{
-		ListKinds:      listKinds,
-		Objects:        []*unstructured.Unstructured{dsc, llsd, cm},
-		CurrentVersion: "2.25.2",
-		TargetVersion:  "3.3.0",
-	})
-
-	chk := llamastack.NewConfigCheck()
-	res, err := chk.Validate(t.Context(), target)
-
-	g.Expect(err).ToNot(HaveOccurred())
-
-	// Find ConfigMapValid condition
-	var cmCond *result.Condition
-	for i := range res.Status.Conditions {
-		if res.Status.Conditions[i].Type == "ConfigMapValid" {
-			cmCond = &res.Status.Conditions[i]
-
-			break
-		}
-	}
-
-	g.Expect(cmCond).ToNot(BeNil())
-	g.Expect(cmCond.Status).To(Equal(metav1.ConditionFalse))
-	g.Expect(cmCond.Impact).To(Equal(result.ImpactBlocking))
-	g.Expect(cmCond.Message).To(ContainSubstring("missing config.yaml"))
-}
-
-func TestLlamaStackConfigCheck_InvalidYAMLSyntax(t *testing.T) {
-	g := NewWithT(t)
-
-	envVars := map[string]string{
-		"VLLM_URL":           "http://vllm:8000",
-		"POSTGRES_HOST":      "postgres.default.svc",
-		"POSTGRES_PASSWORD":  "password",
-		"VLLM_EMBEDDING_URL": "http://embedding:8000",
-	}
-
-	// ConfigMap with invalid YAML syntax
-	invalidYAML := "this is not valid YAML: [\n  unclosed bracket"
-	dsc := newDSC(map[string]string{"llamastackoperator": "Managed"})
-	cm := newConfigMapWithContent("test-config", "test-ns", true, invalidYAML)
-	llsd := newLLSD("test-llsd", "test-ns", envVars, "test-config")
-
-	target := testutil.NewTarget(t, testutil.TargetConfig{
-		ListKinds:      listKinds,
-		Objects:        []*unstructured.Unstructured{dsc, llsd, cm},
-		CurrentVersion: "2.25.2",
-		TargetVersion:  "3.3.0",
-	})
-
-	chk := llamastack.NewConfigCheck()
-	res, err := chk.Validate(t.Context(), target)
-
-	g.Expect(err).ToNot(HaveOccurred())
-
-	// Find ConfigMapValid condition
-	var cmCond *result.Condition
-	for i := range res.Status.Conditions {
-		if res.Status.Conditions[i].Type == "ConfigMapValid" {
-			cmCond = &res.Status.Conditions[i]
-
-			break
-		}
-	}
-
-	g.Expect(cmCond).ToNot(BeNil())
-	g.Expect(cmCond.Status).To(Equal(metav1.ConditionFalse))
-	g.Expect(cmCond.Impact).To(Equal(result.ImpactBlocking))
-	g.Expect(cmCond.Message).To(ContainSubstring("not parseable YAML"))
-}
-
-func TestLlamaStackConfigCheck_DeprecatedBedrockFormat(t *testing.T) {
-	g := NewWithT(t)
-
-	envVars := map[string]string{
-		"VLLM_URL":              "http://vllm:8000",
-		"POSTGRES_HOST":         "postgres.default.svc",
-		"POSTGRES_PASSWORD":     "password",
-		"VLLM_EMBEDDING_URL":    "http://embedding:8000",
-		"AWS_ACCESS_KEY_ID":     "old-key-id", // Deprecated format
-		"AWS_SECRET_ACCESS_KEY": "old-secret",
-	}
-
-	dsc := newDSC(map[string]string{"llamastackoperator": "Managed"})
-	cm := newConfigMap("test-config", "test-ns", true)
-	llsd := newLLSD("test-llsd", "test-ns", envVars, "test-config")
-
-	target := testutil.NewTarget(t, testutil.TargetConfig{
-		ListKinds:      listKinds,
-		Objects:        []*unstructured.Unstructured{dsc, llsd, cm},
-		CurrentVersion: "2.25.2",
-		TargetVersion:  "3.3.0",
-	})
-
-	chk := llamastack.NewConfigCheck()
-	res, err := chk.Validate(t.Context(), target)
-
-	g.Expect(err).ToNot(HaveOccurred())
-
-	// Find BedrockConfigCompatible condition
-	var bedrockCond *result.Condition
-	for i := range res.Status.Conditions {
-		if res.Status.Conditions[i].Type == "BedrockConfigCompatible" {
-			bedrockCond = &res.Status.Conditions[i]
-
-			break
-		}
-	}
-
-	g.Expect(bedrockCond).ToNot(BeNil())
-	g.Expect(bedrockCond.Status).To(Equal(metav1.ConditionFalse))
-	g.Expect(bedrockCond.Impact).To(Equal(result.ImpactBlocking))
-	g.Expect(bedrockCond.Message).To(ContainSubstring("deprecated AWS Bedrock configuration"))
+	// Verify impacted objects
+	g.Expect(res.ImpactedObjects).To(HaveLen(2))
+	g.Expect(res.ImpactedObjects[0].Name).To(Equal("llsd-1"))
+	g.Expect(res.ImpactedObjects[0].Namespace).To(Equal("test-ns"))
+	g.Expect(res.ImpactedObjects[0].Annotations).To(HaveKeyWithValue("upgrade.action", "requires-recreation"))
+	g.Expect(res.ImpactedObjects[1].Name).To(Equal("llsd-2"))
+	g.Expect(res.ImpactedObjects[1].Namespace).To(Equal("test-ns"))
 }
 
 func TestLlamaStackConfigCheck_Metadata(t *testing.T) {
@@ -461,7 +193,9 @@ func TestLlamaStackConfigCheck_Metadata(t *testing.T) {
 	chk := llamastack.NewConfigCheck()
 
 	g.Expect(chk.ID()).To(Equal("workloads.llamastack.config"))
-	g.Expect(chk.Name()).To(Equal("Workloads :: LlamaStack :: Configuration (3.3)"))
+	g.Expect(chk.Name()).To(Equal("Workloads :: LlamaStack :: Upgrade Preparation (2.x to 3.3)"))
 	g.Expect(chk.Group()).To(Equal(check.GroupWorkload))
 	g.Expect(chk.Description()).ToNot(BeEmpty())
+	g.Expect(chk.CheckKind()).To(Equal("llamastackdistribution"))
+	g.Expect(chk.CheckType()).To(Equal("config"))
 }
